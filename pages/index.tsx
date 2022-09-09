410d8be47b8d311074839ea75bedd1d6e0bb446c
Burak Özkırdeniz <burakozkirdeniz@gmail.com>
2022-09-09 16:12:17 +0300

    env variables added

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home = () => {
     let pageKey = newPageKey;
     let nfts; 
     console.log("fetching nfts");
-    const api_key = ""
+    const api_key = process.env.API;
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
         method: 'GET'
@@ -48,7 +48,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = process.env.API;
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}&startToken=${startToken}`;
       const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
